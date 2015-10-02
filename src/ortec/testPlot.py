d7b301b56166b36fc0404cc82fb99e3df9e1b2da
BAASwanson <mark.swanson@berkeleyappliedanalytics.com>
2015-10-02 12:10:07 -0700

    Added Colormap to testPlot
    added a colormap plot to the script

--- src/ortec/testPlot.py
+++ src/ortec/testPlot.py
@@ -1,8 +1,9 @@
 import os
 import h5py
 import matplotlib.pyplot as plt
+from matplotlib.colors import PowerNorm, LogNorm
 import numpy as np
-fName=os.path.abspath('./DataLog-2015-09-29T15-43-35Z.h5')
+fName=os.path.abspath('./DataLog-2015-09-16T19-28-55Z.h5')
 
 data={}
 with h5py.File(fName,'r') as f:
@@ -14,6 +15,19 @@ with h5py.File(fName,'r') as f:
 print data.keys()
 
 f,ax=plt.subplots()
+
+#Colormap plot
+fig1={}
+ax1={}
+plot={}
+for folder in data.keys():
+    fig1[folder],ax1[folder]=plt.subplots()
+    h = ax1[folder].pcolorfast(data[folder]['spectrum'], cmap='rainbow', norm=PowerNorm(.5))
+    plot[folder] = plt.colorbar(h,ax=ax1[folder])
+
+plt.show()
+
+# time vs. sum
 for sn in data.keys():
     t=data[sn]['time']
     gGC=np.sum(data[sn]['spectrum'],axis=1)
