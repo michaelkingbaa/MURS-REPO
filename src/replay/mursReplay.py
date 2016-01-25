05e9230db23c4a74c197c73b9b72e55a6e7b6c04
Dan Chivers <dan.chivers@berkeleyappliedanalytics.com>
2016-01-25 11:22:50 -0800

    Added client and topic to replay init

--- src/replay/mursReplay.py
+++ src/replay/mursReplay.py
@@ -5,17 +5,17 @@ from messaging.mursavro import mursArrayMessage
 
 class mursArrayReplay:
 
-    def __init__(self,h5File):
+    def __init__(self,h5File, client, topic):
         self.h5File = reader(h5File)
         self.sn = self.h5File.getSerialNumbers()
-        self.mursMessage = mursArrayMessage('messaging/mursavro.avsc')
+        self.mursMessage = mursArrayMessage('messaging/mursavro.avsc', client, topic)
 
     def replay(self):
 
         for i in range(self.h5File.getRecordLength()):
             data =  self.h5File.getDataRecord(i)
             print data
-            self.mursMessage.publishMessage(data,'localhost:2181','/murs/sensorArrayReport')
+            self.mursMessage.publishMessage(data)
 
 
 
